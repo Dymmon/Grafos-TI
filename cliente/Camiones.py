ca1e3cdea8c74e7fc2f88e0aa033feb2b15cab57
Carlos Javier <carlos.montuyaog@utem.cl>
2020-08-02 03:47:37 -0400

    la hoja de ruta we

--- cliente/Camiones.py
+++ cliente/Camiones.py
@@ -7,11 +7,10 @@ class Estructura_Ordenada:
         print("coordenada Y:",self.Y)
 
 class Camion:
-    def __init__(self):
-        self.centro_dist
-        self.ptos_venta=[]
-        self.recorrido
-        self.productos
+    def __init__(self,centro,ptos,prod):
+        self.centro_dist=centro
+        self.ptos_venta=ptos
+        self.productos=prod
         
     def mostrar(self):
         print("centro de distribución:",self.centro_dist)
@@ -67,3 +66,39 @@ def leer_archivo():
         i+=1
     return centros,puntos
     
+def buscar_id(centros,cent):
+    i=0
+    while(i<len(centros)):
+        if(centros[i]==cent):
+            return i
+        i+=1
+def distancia(A,B):
+    aux=(((A[0]-B[0])*(A[0]-B[0])) + ((A[1]-B[1])*(A[1]-B[1])))
+    aux= aux**(1/2.0)
+    return aux
+
+def mas_cercano(inicio,recorridos,puntos):
+    first=True
+    i=0
+    while(i<len(puntos)):
+        if(not first and aux>distancia(inicio,puntos[i]) and (puntos[i] not in recorridos)):
+            aux=(distancia(inicio,puntos[i]))
+            siguiente=puntos[i]
+        if(first):
+            aux=(distancia(inicio,puntos[i]))
+            siguiente=puntos[i]
+            first=False
+        i+=1
+    recorridos.append(siguiente)
+    return siguiente
+            
+def crear_ruta(camion,centros,puntos):
+    #Técnica matemática "Programación dinámica"
+    orden=[]
+    recorridos=[]
+    orden.append("Centro de distribucion " + str(buscar_id(centros,camion.centro_dist)))
+    orden.append("punto de venta " + str(buscar_id(puntos,mas_cercano(camion.centro_dist,recorridos,camion.ptos_venta))))
+    while( len(recorridos)< len(camion.ptos_venta)):
+        orden.append("Punto de venta " + str(buscar_id(puntos,mas_cercano(recorridos[len(recorridos)-1],recorridos,camion.ptos_venta))))
+    orden.append("A mimir señor camion")
+    #no se que hacer con los productos we, aiura
