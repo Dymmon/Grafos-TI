9ac37cdfbcb0970118e585bb9ae969210f5c1006
Jorge Verdugo <jorgeverdugoch@gmail.com>
2020-08-03 22:27:50 -0400

    Se reestructura alguna funciones

--- servicio/Ruta.py
+++ servicio/Ruta.py
@@ -53,23 +53,25 @@ class Ruta:
         return resultado
 
     # Calcula la distancia total en una ruta
-    def distancia_total(self):
+    @staticmethod
+    def distancia_total(puntos):
         recorridos = []
-        recorridos.append(self.centro)
+        recorridos.append(puntos)
         total = 0
 
-        for punto in self.puntos:
-            total += self.distancia_entre(recorridos[-1].coordenadas,
+        for punto in puntos:
+            total += Ruta.distancia_entre(recorridos[-1].coordenadas,
                                           punto.coordenadas)
             recorridos.append(punto)
 
         return total
 
     # Obtiene el punto más cercano a un punto definido
-    def mas_cercano(self, inicial, recorridos=[]):
+    @staticmethod
+    def mas_cercano(inicial, puntos, recorridos=[]):
         primero = True
-        for punto in self.puntos:
-            nueva_distancia = self.distancia_entre(inicial, punto.coordenadas)
+        for punto in puntos:
+            nueva_distancia = Ruta.distancia_entre(inicial, punto.coordenadas)
             if (primero or (menor > nueva_distancia and (punto not in recorridos))):
                 siguiente = punto
                 menor = nueva_distancia
@@ -79,3 +81,74 @@ class Ruta:
         if (siguiente not in recorridos):
             recorridos.append(siguiente)
         return siguiente
+
+    @staticmethod
+    def candidato(inicio, disponibles, maximo):
+        orden = [inicio]
+        recorridos = [inicio]
+
+        cantidad = inicio.productos
+        disponibles_actual = []
+
+        for disponible in disponibles:
+            if (disponible not in recorridos):
+                disponibles_actual.append(disponible)
+
+        while (cantidad < maximo):
+            cercano = Ruta.mas_cercano(
+                recorridos[-1], disponibles_actual, recorridos)
+            if (cercano != None):
+                cantidad += recorridos[-1].productos
+            else:
+                cantidad += maximo
+            if (cantidad <= maximo):
+                if (cercano not in orden):
+                    orden.append(cercano)
+
+                disponibles_actual = []
+                for disponible in disponibles:
+                    if (disponible not in recorridos):
+                        disponibles_actual.append(disponible)
+
+        a = Ruta.distancia_total(orden)
+        return orden, a
+
+    @staticmethod
+    def seleccion_pv(maximo, puntos, recorridos):
+        disponibles = []
+        for punto in puntos:
+            if (punto not in recorridos and punto != None):
+                disponibles.append(punto)
+        if (disponibles == []):
+            return None
+
+        primero = True
+        ruta = []
+        largo = 0
+
+        for disponible in disponibles:
+            ruta_actual = []
+            largo_actual = 0
+            ruta_actual, largo_actual = Ruta.candidato(
+                disponible, disponibles, maximo)
+            if (primero or largo_actual > largo):
+                ruta = ruta_actual
+                largo = largo_actual
+
+        return ruta
+
+
+def crear_ruta(self):
+    # Técnica matemática "Programación dinámica"
+    '''camion, centros, cant_puntos, recorridos, puntos
+    self.puntos = seleccion_pv(camion.productos, recorridos, cant_puntos)
+    orden = []
+    orden.append("Centro de distribucion " +
+                 str(buscar_id(centros, camion.centro_dist)))
+    i = 0
+    while (i < len(camion.ptos_venta)):
+        orden.append("Punto de venta " +
+                     str(buscar_id(puntos, camion.ptos_venta[i])))
+        i += 1
+    orden.append("A mimir señor camion")
+    '''
