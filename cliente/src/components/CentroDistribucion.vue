477128f4d76d7e54b027354b86fcfa5f1e258f78
PollitoMayo <jp.vergaran@gmail.com>
2020-08-03 22:22:46 -0400

    Se modifica Stepper

--- cliente/src/components/CentroDistribucion.vue
+++ cliente/src/components/CentroDistribucion.vue
@@ -1,40 +1,76 @@
 <template>
   <div class="fill-height d-flex align-center justify-center">
-  <v-col align="center" justify="space-around" class="fill-height">
-    <h3>Ingresar archivo</h3>
-    <v-file-input
-      v-model="files"
-      color="deep-purple accent-4"
-      placeholder="Seleccionar archivo"
-      prepend-icon="mdi-paperclip"
-      outlined
-      :show-size="1000"
-    >
-    </v-file-input>
-    <v-row align="center" justify="space-between">
-    <v-btn outlined rounded @click="devolverse">Atrás</v-btn>
-    <v-btn outlined rounded @click="continuar">Siguiente</v-btn>
-    </v-row>
-  </v-col>
+    <v-col align="center" justify="space-around" class="fill-height">
+      <v-row>
+        <v-col cols="6"><h3>Número de camiones</h3></v-col>
+        <v-col cols="2">
+          <v-btn rounded class="ma-0 pa-0" @click="cantidad = cantidad - 1"
+            >-</v-btn
+          >
+        </v-col>
+        <v-col cols="2">
+          <v-text-field outlined v-model="cantidad"></v-text-field>
+        </v-col>
+        <v-col cols="2">
+          <v-btn rounded class="ma-0 pa-0" @click="cantidad = cantidad + 1"
+            >+</v-btn
+          >
+        </v-col>
+      </v-row>
+      <v-divider></v-divider>
+      <div>
+        <div v-for="(centro, i) in centros" :key="i">
+        <v-row>
+          <v-col cols="3"><h3>CD{{centro.id}}</h3></v-col>
+          <v-col cols="4">
+            <v-select :items="camiones" item-text="nombre" dense outlined></v-select>
+          </v-col>
+        </v-row>
+        </div>
+      </div>
+      <v-divider></v-divider>
+      <v-row align="center" justify="space-between">
+        <v-btn outlined rounded @click="devolverse">Atrás</v-btn>
+        <v-btn outlined rounded @click="continuar">Siguiente</v-btn>
+      </v-row>
+    </v-col>
   </div>
 </template>
 
 <script>
 export default {
   name: "input-archivo",
-  props: ["onSiguiente", "onDevolverse"],
+  props: ["onSiguiente", "onDevolverse", "centros", "puntos"],
   data() {
     return {
-      files: null,
+      cantidad: this.centros.length,
+      camiones: [],
+      seleccionCamiones: []
     };
   },
+  watch: {
+    centros() {
+      this.cantidad = this.centros.length;
+    },
+    cantidad(){
+        let auxiliar = [];
+        for (let index = 1; index <= this.cantidad; index++) {
+            let dato = {
+                nombre: "Camión " + index
+            };
+            auxiliar.push(dato);
+        }
+        this.camiones = auxiliar;
+        console.log("CAMIONES: ", this.camiones);
+    }
+  },
   methods: {
-      devolverse(){
-          this.onDevolverse();
-      },
-      continuar(){
-          this.onSiguiente();
-      }
-  }
+    devolverse() {
+      this.onDevolverse();
+    },
+    continuar() {
+      this.onSiguiente();
+    },
+  },
 };
 </script>
--- cliente/src/components/InputArchivo.vue
+++ cliente/src/components/InputArchivo.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="fill-height d-flex align-center justify-center">
-  <v-col align="center" justify="space-around" class="fill-height">
-    <h3>Ingresar archivo</h3>
-    <v-file-input
-      v-model="archivo"
-      color="deep-purple accent-4"
-      placeholder="Seleccionar archivo"
-      prepend-icon="mdi-paperclip"
-      outlined
-      show-size
-      accept=".txt"
-    >
-    </v-file-input>
-    <v-row align="center" justify="space-between">
-    <v-btn outlined rounded @click="onCancelar">Cancelar</v-btn>
-    <v-btn outlined rounded @click="continuar">Siguiente</v-btn>
-    </v-row>
-  </v-col>
+    <v-col align="center" justify="space-around" class="fill-height">
+      <h3>Ingresar archivo</h3>
+      <v-file-input
+        v-model="archivo"
+        color="deep-purple accent-4"
+        placeholder="Seleccionar archivo"
+        prepend-icon="mdi-paperclip"
+        outlined
+        show-size
+        accept=".txt"
+      >
+      </v-file-input>
+      <v-row align="center" justify="space-between">
+        <v-btn outlined rounded @click="onCancelar">Cancelar</v-btn>
+        <v-btn outlined rounded @click="continuar">Siguiente</v-btn>
+      </v-row>
+    </v-col>
   </div>
 </template>
 
@@ -24,24 +24,29 @@
 import axios from "axios";
 export default {
   name: "input-archivo",
-  props: ["onSiguiente", "onCancelar"],
+  props: ["onSiguiente", "onCancelar", "onActualizar"],
   data() {
     return {
       archivo: null,
     };
   },
   watch: {
-      archivo(){console.log("ARCHIVO: ", this.archivo);}
   },
   methods: {
-      continuar(){
-        //   let formData = new FormData();
-        //   formData.append('archivo', this.archivo);
-        //   console.log("AAA", formData.getAll('archivo'));
-        // axios.post(this.$apiUrl)
-        
-        //   this.onSiguiente();
-      }
-  }
+    continuar() {
+      let formData = new FormData();
+      formData.append("archivo", this.archivo);
+      axios
+        .post(`${this.$apiUrl}/resumen`, formData)
+        .then((r) => {
+          console.log("DATA: ", r.data);
+          this.onActualizar(r.data.puntosVenta, r.data.centrosDistribucion);
+          this.onSiguiente();
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+  },
 };
 </script>
